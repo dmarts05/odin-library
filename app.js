714825585cdcfe662f4eb23f943cfd5b5dd97b65
dmarts05 <91263391+dmarts05@users.noreply.github.com>
2022-06-25 18:55:01 +0200

    Correct grammar error

--- app.js
+++ app.js
@@ -135,7 +135,7 @@ window.onclick = (e) => {
   }
 };
 
-// Hide modal when when by pressing Esc key
+// Hide modal by pressing Esc key
 document.addEventListener('keydown', (e) => {
   if (e.key === 'Escape') {
     modal.classList.remove('show');
